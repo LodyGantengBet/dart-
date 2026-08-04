9dc11d6f2fcade0c6e0467b53d1b689708d4a48f
Lody Seveninc <lodyrpl2@gmail.com>
2026-08-04 12:20:03 +0700

    LKPD-2 kontrol alur

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,69 @@
-
+import 'package:intl/intl.dart';
 void main() {
-  // Data barang
   String namaBarang = "Buku Tulis";
-  double hargaAnggota = 3000.0;
-  double hargaUmum = 3500.0;
+  double hargaAnggota = 12500.0;
+  double hargaUmum = 15000.0;
   int jumlahStok = 40;
-  bool tersedia = true;
-
-  int jumlahBeli = 3;
-  double totalAnggota = jumlahBeli * hargaAnggota;
-  double totalUmum = jumlahBeli * hargaUmum;
-  double selisih = totalUmum - totalAnggota;
-  print("=== KARTU DATA BARANG ===");
-  print("Nama : $namaBarang");
-  print("Harga Anggota : Rp$hargaAnggota");
-  print("Harga Umum : Rp$hargaUmum");
+  bool anggota = true;
+  String kategori = "atk";
+  bool tersedia;
+  if (jumlahStok == 0) {
+    tersedia = false;
+  } else {
+    tersedia = true;
+  }
+  double harga;
+  if (anggota) {
+    harga = hargaAnggota;
+  } else {
+    harga = hargaUmum;
+  }
+  int jumlahBeli = 10;
+  double total = harga * jumlahBeli;
+  double persenPotongan;
+  if (total > 200000) {
+    persenPotongan = 0.10;
+  } else if (total > 100000) {
+    persenPotongan = 0.05;
+  } else {
+    persenPotongan = 0.0;
+  }
+  double potongan = total * persenPotongan;
+  double hargaAkhir = total - potongan;
+  String rak;
+  switch (kategori) {
+    case "atk":
+      rak = "Rak 1";
+      break;
+    case "makanan":
+      rak = "Rak 2";
+      break;
+    case "minuman":
+      rak = "Rak 3";
+      break;
+    default:
+      rak = "Rak lain";
+  }
+  final formatRupiah = NumberFormat('#,##0', 'id_ID');
+  print("=== DATA KOPERASI ===");
+  print("Nama Barang : $namaBarang");
+  print("Kategori : $kategori");
+  print("Rak : $rak");
+  print("Harga Anggota : Rp${formatRupiah.format(hargaAnggota)}");
+  print("Harga Umum : Rp${formatRupiah.format(hargaUmum)}");
   print("Stok : $jumlahStok");
   print("Tersedia : $tersedia");
-  print("Total (anggota) $jumlahBeli pcs: Rp$totalAnggota");
-  print("Selisih vs umum : Rp$selisih");
+  print("=== TRANSAKSI ===");
+  print("Status Anggota : $anggota");
+  print("Jumlah Beli : $jumlahBeli");
+  print("Harga Satuan : Rp${formatRupiah.format(harga)}");
+  print("Total Sebelum Potongan : Rp${formatRupiah.format(total)}");
+  print("Potongan : ${persenPotongan * 100}%");
+  print("Nilai Potongan : Rp${formatRupiah.format(potongan)}");
+  print("Harga Akhir : Rp${formatRupiah.format(hargaAkhir)}");
+  // Switch lebih rapi daripada banyak if karena digunakan untuk
+  // membandingkan satu variabel dengan beberapa pilihan nilai yang tetap.
+  // Dengan switch, setiap kategori memiliki case sendiri sehingga kode
+  // lebih mudah dibaca dan dikelola.
 }
-// Mengapa pemilihan tipe data pada program ini penting bagi keakuratan kasir koperasi?
-// Pemilihan tipe data yang tepat membuat harga dan jumlah stok dapat dihitung dengan benar.
-// Kesalahan tipe data dapat menyebabkan perhitungan harga atau stok menjadi tidak akurat.
+
